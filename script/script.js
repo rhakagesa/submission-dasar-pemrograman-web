c2bcfec252261228c66c12cb0fe7c3dfb1f3e205
rhakagesa <rhakagesa.gmail.com>
2023-09-17 00:35:17 +0700

    Menambahkan animasi saat scroll

--- script/script.js
+++ script/script.js
@@ -13,3 +13,16 @@ closeButton.addEventListener("click", function () {
   hamburgerMenu.classList.toggle("hamburger-active");
   closeButton.classList.toggle("active");
 });
+
+// Menyembunyikan Header Saat Scroll Down
+let lastState = window.scrollY;
+window.onscroll = function () {
+  let currentState = window.scrollY;
+  if (currentState < lastState) {
+    document.getElementById("header").style.top = "0";
+    document.getElementById("header").style.transition = "all 0.5s";
+  } else {
+    document.getElementById("header").style.top = "-5rem";
+  }
+  lastState = currentState;
+};
